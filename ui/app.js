cf057b1424c6ef109ccb4c71f429e7b2ab226c25
Madalina - Andreea <madalina.pascariu0305@gmail.com>
2021-02-11 11:26:14 +0100

    push

--- ui/app.js
+++ ui/app.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+// other imports
+const expressHandlebars = require('express-handlebars');
+
+const app = express();
+
+app.enable('etag');
+app.engine('hbs', expressHandlebars({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Other middleware follows below...
+module.exports.app = app;
